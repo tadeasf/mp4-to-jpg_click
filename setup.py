1b3f19603001e24422c3ae1bc1d21c6dc7423c18
tadeasf <taddy.fort@gmail.com>
2024-05-16 15:51:59 +0200

    pypy package binary

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mp4_to_jpg",
+    version="1.0.0",
+    author="Tadeas Fort",
+    author_email="business@tadeasfort.com",
+    description="A tool to extract frames from MP4 videos and remove duplicates",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tadeasf/mp4-to-jpg_click",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+        "opencv-python",
+        "tk",
+        "tqdm",
+        "loguru",
+        "imagededup",
+    ],
+    entry_points={
+        "console_scripts": [
+            "mp4-to-jpg = mp4_to_jpg.script:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
